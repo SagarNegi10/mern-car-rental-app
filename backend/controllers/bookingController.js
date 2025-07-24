9aa6902f98f7225c764e52462de4907221cf889d
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-07-24 12:33:33 +0530

    add bookings API endpoints

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,119 @@
+import Booking from "../models/Booking.js"
+import Car from "../models/Car.js"
+
+// Function to check Availablilty of Car for a given Date
+const checkAvailability = async (car, pickupDate, returnDate) => {
+    const bookings = await Booking.find({
+        car, 
+        pickupDate: {$lte: returnDate}, 
+        returnDate: {$gte: pickupDate},
+    })
+    return bookings.length === 0
+}
+
+// API to check Availablilty of Car for a given Date and location
+export const checkAvailabilityOfCar = async (req, res) => {
+    try {
+        const {loaction, pickupDate, returnDate} = req.body
+        // fetch all available cars for the given loaction
+        const cars = await Car.find({location, isAvailable: true})
+
+        // check car availability for the given date range using function
+        const checkAvailableCarsPromises = cars.map(async (car) => {
+           const isAvailable =  await checkAvailability(car._id, pickupDate, returnDate)
+           return {...car._doc, isAvailable: isAvailable}
+        })
+
+        let availableCars = await Promise.all(checkAvailableCarsPromises);
+        availableCars = availableCars.filter(car => car.isAvailable === true)
+
+        res.json({sucsses: true, availableCars})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucsses: false, message: error.message})
+    }
+}
+
+// API to create booking
+export const createBooking = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const{car, pickupDate, returnDate} = req.body;
+
+        const isAvailable = await checkAvailability(car, pickupDate, returnDate)
+
+        if(!isAvailable){
+            return res.json({sucsses: false, message: "Car is not Available"})
+        }
+
+        const carData = await Car.findById(car)
+
+        // Calculate price based on pickupdate and returndate
+        const picked = new Date(pickupDate);
+        const returned = new Date(returnDate);
+        const noOfDays = Math.ceil((returned - picked) / (1000 * 60 * 60 * 24))
+        const price = carData.pricePerDay * noOfDays
+
+        await Booking.create({car, owner: carData.owner, user: _id, pickupDate, returnDate, price})
+
+        res.json({sucsses: true, message: "Booking created"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucsses: false, message: error.message})
+    }
+}
+
+// API to list User Bookings
+export const getUserBookings = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const bookings = await Booking.find({user: _id}).populate("car").sort({createdAt: -1})
+
+        res.json({sucsses: true, bookings})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucsses: false, message: error.message})
+    }
+}
+
+// API to get owner bookings
+export const getOwnerBookings = async (req, res) => {
+    try {
+        if(req.user.role != 'owner'){
+            return res.json({sucsses: false, message: "Unauthorized"})
+        }
+
+        const bookings = await Booking.find({owner: req.user._id}).populate('car user').select("-user.password").sort({createdAt: -1})
+
+        res.json({sucsses: true, bookings})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucsses: false, message: error.message})
+    }
+}
+
+// API to change the booking status
+export const changeBookingStatus = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const {bookingId, status} = req.body
+        const booking = await Booking.findById(bookingId)
+
+        if(booking.owner.toString() !== _id.toString()){
+            return res.json({sucsses: false, message: "Unauthorized"})
+        }
+
+        booking.status = status;
+        await booking.save()
+
+        res.json({sucsses: true, message: "Status Updated"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucsses: false, message: error.message})
+    }
+}
--- backend/controllers/ownerController.js
+++ backend/controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imagekit from "../configs/imageKit.js";
+import Booking from "../models/Booking.js";
 import Car from "../models/Car.js";
 import User from "../models/user.js";
 import fs from 'fs'
@@ -43,6 +44,130 @@ export const addCar = async (req, res) => {
         const image = optimizedImageURL;
         await Car.create({...car, owner: _id, image})
         res.json({success: true, meassge: 'Car Added'})
+
+    } catch (error) {
+        console.log(error.meassge);
+        res.json({success: false, meassge: error.meassge})
+    }
+}
+
+// API to list Owner Cars
+export const getOwnerCars = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const cars = await Car.find({owner: _id})
+        res.json({success: true, cars})
+    } catch (error) {
+        console.log(error.meassge);
+        res.json({success: false, meassge: error.meassge})
+    }
+}
+
+// API to toggle car Availability
+export const toggleCarAvailability = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body;
+        const car = await Car.findById(carId)
+
+        // Checking is car belong to the user
+        if(car.owner.toString() !== _id.toString()){
+            res.json({success: false, meassge: "Unauthorized"})
+        }
+
+        car.isAvailable = !car.isAvailable;
+        await car.save()
+
+        res.json({success: true, meassge: "Availability Toggled"})
+    } catch (error) {
+        console.log(error.meassge);
+        res.json({success: false, meassge: error.meassge})
+    }
+}
+
+// API to Delete a car
+export const deleteCar = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body;
+        const car = await Car.findById(carId)
+
+        // Checking is car belong to the user
+        if(car.owner.toString() !== _id.toString()){
+            res.json({success: false, meassge: "Unauthorized"})
+        }
+
+        car.owner = null;
+        car.isAvailable = false;
+        await car.save()
+
+        res.json({success: true, meassge: "Car Removed"})
+    } catch (error) {
+        console.log(error.meassge);
+        res.json({success: false, meassge: error.meassge})
+    }
+}
+
+// API to get Dashboad data
+export const getDashboardData = async (req, res) => {
+    try {
+        const {_id, role} = req.user;
+        if(role !== 'owner'){
+            res.json({success: false, meassge: "Unauthorized"})
+        }    
+        const cars = await Car.find({owner: _id})
+        const bookings = await Booking.find({owner: _id}).populate('car').sort({createdAt: -1});
+        const pendingBookings = await Booking.find({owner: _id, status: "pending"})
+        const completedBookings = await Booking.find({owner: _id, status: "confirmed"})
+
+        // Calaculate monthlyRevenue from bookings where status is confirmed
+        const monthlyRevenue = bookings.slice().filter(booking => booking.status === ' confirmed').reduce((acc, booking) => acc + booking.price, 0)
+
+        const dashboardData = {
+            totalCars: cars.length,
+            totalBookings: bookings.length,
+            pendingBookings: pendingBookings.length,
+            completedBookings: completedBookings.length,
+            recentBookings: bookings.slice(0,3),
+            monthlyRevenue
+        }
+
+        res.json({success: true, dashboardData})
+
+    } catch (error) {
+        console.log(error.meassge);
+        res.json({success: false, meassge: error.meassge})
+    }
+}
+
+// API to update user Image
+export const updateUserImage = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const imageFile = req.file;
+
+        // Upload Image to ImageKit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/users'
+        })
+
+        // optimization through imagekit URL transformation
+        var optimizedImageURL = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {width: '400'}, // Width resizing
+                {quality: 'auto'}, // Auto compression
+                {format: 'webp'}, // Convert to modern format
+            ]
+        });
+
+        const image = optimizedImageURL;
+        await User.findByIdAndUpdate(_id, {image});
+
+        res.json({success: true, meassge: 'Image Updated'})
 
     } catch (error) {
         console.log(error.meassge);
